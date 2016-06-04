6f1a59a8dce8a80e51d0690469464fb69a3d6939
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-06-05 06:40:03 +0900

    Make Editor and Content Factory to React.Component

--- src/js/components/contents/Content.js
+++ src/js/components/contents/Content.js
@@ -2,27 +2,32 @@ import React from 'react'
 import Blank        from './Blank'
 import Deploy       from './Deploy'
 import FeatureList  from './FeatureList'
+import Feature      from './Feature'
 import Social       from './Social'
 import Title        from './Title'
 import Workflow     from './Workflow'
 
-export default function Content(props){
+export default class Content extends React.Component{
 
-  switch(props.contentType) {
-    case Blank.name :
-      return React.createElement(Blank, props);
-    case Deploy.name :
-      return React.createElement(Deploy, props);
-    case FeatureList.name :
-      return React.createElement(FeatureList, props);
-    case Social.name :
-      return React.createElement(Social, props);
-    case Title.name :
-      return React.createElement(Title, props);
-    case Workflow.name :
-      return React.createElement(Workflow, props);
-    default:
-      return <div>{"No Such Content = " + props.contentType}</div>
+  render() {
+    switch(this.props.contentType) {
+      case Blank.name :
+        return React.createElement(Blank, this.props);
+      case Deploy.name :
+        return React.createElement(Deploy, this.props);
+      case FeatureList.name :
+        return React.createElement(FeatureList, this.props);
+      case Feature.name :
+        return React.createElement(Feature, this.props);
+      case Social.name :
+        return React.createElement(Social, this.props);
+      case Title.name :
+        return React.createElement(Title, this.props);
+      case Workflow.name :
+        return React.createElement(Workflow, this.props);
+      default:
+        return <div>{"No Such Component = " + this.props.contentType}</div>
+    }
   }
 
 }
--- src/js/components/editors/Editor.js
+++ src/js/components/editors/Editor.js
@@ -6,21 +6,23 @@ import TitleEditor        from './TitleEditor'
 import SelectorEditor     from './SelectorEditor'
 import { SHOW_EDITOR, CLOSE_EDITOR } from "../../actions/editorActions"
 
-export default function Editor(props) {
-    switch(props.editorType) {
+export default class Editor extends React.Component {
+  render() {
+    switch(this.props.editorType) {
       case undefined :
         return <div>Closed</div>
       case FeatureListEditor.name :
-        return React.createElement(FeatureListEditor, props);
+        return React.createElement(FeatureListEditor, this.props);
       case FeatureEditor.name :
-        return React.createElement(FeatureEditor, props);
+        return React.createElement(FeatureEditor, this.props);
       case SocialEditor.name :
-        return React.createElement(SocialEditor, props);
+        return React.createElement(SocialEditor, this.props);
       case TitleEditor.name :
-        return React.createElement(TitleEditor, props);
+        return React.createElement(TitleEditor, this.props);
       case SelectorEditor.name :
-        return React.createElement(SelectorEditor, props);
+        return React.createElement(SelectorEditor, this.props);
       default:
-        return <div>{"No Such Editor = " + props.editorType}</div>
+        return <div>{"No Such Editor = " + this.props.editorType}</div>
     }
+  }
 }
