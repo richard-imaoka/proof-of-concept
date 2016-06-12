0bd5f748eea06c008d7d7f2964280d637aa4e5f6
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-06-12 20:58:45 +0900

    Content Refactoring

--- src/js/components/contents/Content.js
+++ src/js/components/contents/Content.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import {showEditor} from '../../actions/editorActions'
+import NoneContent  from './NoneContent'
 
 export default class Content extends React.Component{
   render() {    
-    const type = this.props.data.get("type");
-    
-    if(type === undefined)
-      return <div>{"No Such Component = " + type}</div>;
-    else
-      return React.createElement(type, this.props);
+    const content       = this.props.data.get("type");
+    const ContentViewer = content === undefined ? NoneContent : content;
+
+    return <ContentViewer ref="content" index={this.props.index} store={this.props.store} data={this.props.data} onClick={this.onClick.bind(this)} />
   }
 
+
   onClick() {
-    this.props.store.dispatch( showEditor( this.props.index, this.contentData() ) );
+    const content = this.refs.content;
+
+    if(content.contentData === undefined){
+      window.alert( "ERROR! " + content.name + " does not implement contentData() method. Please do!" );
+      return;
+    }
+    else{
+      this.props.store.dispatch( showEditor( this.props.index, content.contentData() ) );
+    }
   }
+
 }
---
+++ src/js/components/contents/NoneContent.js
@@ -0,0 +1,12 @@
+import React   from 'react'
+
+export default class NoneContent extends React.Component {
+  render() {
+    return <div>None Content</div>
+  }
+
+  contentData(){
+    return {};
+  }
+  
+}
